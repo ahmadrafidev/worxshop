022896ad87007e4730f88131bd81b473a73dfaba
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-11-20 10:10:35 +0700

    feat: finishing personal side project web

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+export function Footer() {
+  return (
+    <footer className="py-6 fixed bottom-0 flex justify-center items-center">
+      <p className="text-xs md:text-sm font-normal text-gray-600 dark:text-gray-300">
+        Built by{" "}
+        <Link href="https://x.com/rafiwiranaa" passHref className="underline" aria-label="Visit Rafi's Twitter">
+          Rafi
+        </Link>
+        .
+      </p>
+    </footer>
+  )
+}
